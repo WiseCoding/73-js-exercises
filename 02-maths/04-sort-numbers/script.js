7b37663e159d531993a3d5db61113a7f6306fc09
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-11 12:16:44 +0200

    EX 2.4 done

---
+++ 02-maths/04-sort-numbers/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /02-maths/04-sort-numbers/script.js - 2.4: classer des numbers
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+  // to get the value of an input: document.getElementById("element-id").value
+
+  document.getElementById('run').addEventListener('click', function () {
+    const numString = document.querySelector('#numbers').value;
+    const numArray = numString.split(',');
+    const sort = numArray.sort((a, b) => a - b);
+    console.log(sort);
+  });
+})();
