d4a3a9bf76cb4c295faf644046acb9f3ffea0630
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-14 11:35:21 +0200

    EX 4.2 done

--- 04-dates/02-title-by-hour-two/script.js
+++ 04-dates/02-title-by-hour-two/script.js
@@ -9,10 +9,42 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // Change greeting message if our input time is smaller than the actual time
+  function greeting(inpTime) {
+    // get current hour and minutes
+    now = getFullTime();
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    let message;
 
-    // your code here
+    if (now < inpTime) {
+      message = `Have a good day!`;
+    } else {
+      message = `Enjoy your evening!`;
+    }
 
+    document.querySelector('#target').textContent = message;
+  }
+
+  // Add 0 in front of input if smaller then 10
+  function checkTime(time) {
+    if (time < 10) {
+      time = '0' + time;
+    }
+    return time;
+  }
+
+  // Get current time, and convert it to a full number
+  function getFullTime() {
+    const date = new Date();
+    let h = date.getHours().toString();
+    let m = date.getMinutes().toString();
+    h = checkTime(h);
+    m = checkTime(m);
+    let fullTime = h + m;
+    fullTime = Number(fullTime);
+    return fullTime;
+  }
+
+  greeting(1730);
 })();
