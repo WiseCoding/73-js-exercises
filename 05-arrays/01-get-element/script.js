3f33d39cb03191b70015b0998b8f40d1b995edcf
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-15 12:19:27 +0200

    EX 5.1 done

--- 05-arrays/01-get-element/script.js
+++ 05-arrays/01-get-element/script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
+ * /05-arrays/01-get-element/script.js - 5.1: array element
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -9,23 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
-
+(function () {
+  var fruits = [
+    'apple',
+    'pear',
+    'raspberry',
+    'tomatoes',
+    'kiwi',
+    'banana',
+    'orange',
+    'mandarin',
+    'durian',
+    'peach',
+    'grape',
+    'cherry',
+  ];
+  document.querySelector('#run').addEventListener('click', function () {
+    console.log(fruits[3]);
+    alert(fruits[3]);
+  });
 })();
