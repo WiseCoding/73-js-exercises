4a334b95346339acc1d276b305808464ff0dd056
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-18 11:52:36 +0200

    EX 6.1 done

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /06-objects/01-generate-object/script.js - 6.1: générer un object
+ * /06-objects/01-generate-object/script.js - 6.1: generate un object
  *
  * coded by leny@BeCode
  * started at 08/05/2019
@@ -10,5 +10,19 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  const me = [
+    {
+      lastname: 'Bwontie',
+      firstname: 'Mattias',
+      age: 99,
+      city: 'Gent',
+      country: 'Belgium',
+    },
+  ];
+  document.querySelector('#run').onclick = () => {
+    console.log(
+      `Hello! My name is ${me[0].firstname} ${me[0].lastname}, I am ${me[0].age} years old and live in ${me[0].country} in the city of ${me[0].city}.`
+    );
+    console.log(me);
+  };
 })();
