81a0773c9b848f61eb6a6b662001249dede905f5
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-22 16:30:00 +0200

    EX 9.1 done

---
+++ 09-misc/01-waving-text/script.js
@@ -0,0 +1,59 @@
+/* becode/javascript
+ *
+ * /07-misc/01-waving-text/script.js - 7.1: effet vague
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+  // Reset field
+  let target = document.querySelector('#target');
+  target.textContent = '';
+
+  // Add span to animate
+  const span = document.createElement('SPAN');
+  span.textContent = 'lorem wave';
+  target.appendChild(span);
+
+  // Animation
+  let id = setInterval(frame, 100);
+  let size = 1;
+  let growth = 'grow';
+
+  // Function
+  function frame() {
+    let fontSize = '1rem';
+
+    //size
+    if (size === 10 && growth === 'grow') {
+      growth = 'shrink';
+    }
+    if (size === 1 && growth === 'shrink') {
+      growth = 'grow';
+    }
+
+    if (growth === 'grow') {
+      size++;
+    } else {
+      size--;
+    }
+    fontSize = `${size}rem`;
+    span.style.fontSize = fontSize;
+
+    //color
+    const color1 = ~~(Math.random() * 250) + 1;
+    const color2 = ~~(Math.random() * 250) + 1;
+    const color3 = ~~(Math.random() * 250) + 1;
+    fontColor = `rgb(${color1},${color2},${color3})`;
+    span.style.color = fontColor;
+  }
+
+  // Cancel animation on click
+  document.querySelector('#target').onclick = () => {
+    clearInterval(id);
+  };
+})();
---
+++ 10-asynchronous/01-get-posts/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /10-asynchronous/01-get-posts/script.js - 10.1: chargement d'articles
+ *
+ * coded by leny@BeCode
+ * started at 09/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+  function myCallbackFunction(error, articles) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(articles);
+  }
+
+  window.lib.getPosts(myCallbackFunction);
+})();
