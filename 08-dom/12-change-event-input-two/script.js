80d699797787a384c95b14fd1c99cae9a532940f
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-23 13:04:24 +0200

    EX 8.12 done

--- 08-dom/12-change-event-input-two/script.js
+++ 08-dom/12-change-event-input-two/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  const pass1 = document.querySelector('#pass-one');
+  const validity = document.querySelector('#validity');
 
-    // your code here
+  // Listen for keyup
+  pass1.onkeyup = () => {
+    const val1 = pass1.value;
+    const length = val1.length;
+    let num = 0;
 
+    // Check numbers in input
+    for (let i = 0; i < val1.length; i++) {
+      if (!isNaN(val1[i])) {
+        num++;
+      }
+    }
+
+    // Validate: at least 8 characters & at least 2 numbers
+    if (length >= 8 && num >= 2) {
+      validity.textContent = `ok`;
+    } else {
+      validity.textContent = `Not ok`;
+    }
+  };
 })();
